6ccd23bcfac67c1bfd3a285a00881ede585624f4
Niko Kozak <nikokozak@gmail.com>
2024-11-12 11:33:40 -0500

    add pseudocode to main cycle

--- mic/mic.py
+++ mic/mic.py
@@ -4,9 +4,19 @@ import pyaudio
 
 p = pyaudio.PyAudio()
 
+def run_system(config):
+    pass
+    # breathe cycle on repeat.
+        # within breathe cycle, IF on inhale, check for peaks in DB.
+        # IF peak, trigger inflation MAX, until time of inhale cycle ends.
+        # IF on exhale, previous inhale recorded audio, play back exhale.
+
 if __name__ == "__main__":
     if initialize_serial():
-        menu()
+        # The following is pseudo-code until we implement it.
+
+        menu() # configuration menu, should return a configuration object
+        # run_system(config) # main loop
     else:
         print("Unable to connect to Arduino. Exiting.")
     p.terminate()
